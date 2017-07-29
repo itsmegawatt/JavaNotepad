a78e851723c727327641f5420b1ee8e080904400
Megg Gawat <megawatt@live.com>
2017-07-29 11:47:17 -0400

    importing modules
    imported gui files and io files

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -1,5 +1,11 @@
 package notepad;
 
+import javax.swing.*; // for the main JFrame design
+import java.awt.*; // for the GUI
+import java.awt.event.*; // for event handling
+import java.util.Scanner; // for reading from a file
+import java.io.*; // for writing to a file
+
 public class Notepad {
 
 }
