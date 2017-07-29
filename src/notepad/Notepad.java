57c953e8d796df0d7b925cc9517cee06be2c52a7
Megg Gawat <megawatt@live.com>
2017-07-29 11:55:23 -0400

    Created first class variables

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -6,6 +6,11 @@ import java.awt.event.*; // for event handling
 import java.util.Scanner; // for reading from a file
 import java.io.*; // for writing to a file
 
-public class Notepad {
-
+public class Notepad extends JFrame implements ActionListener {
+	private TextArea textArea = new TextArea("", 0,0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+	private MenuBar menuBar = new MenuBar();
+	private Menu file = new Menu();
+	private MenuItem openFile = new MenuItem();
+	private MenuItem saveFile = new MenuItem();
+	private MenuItem close = new MenuItem();
 }
