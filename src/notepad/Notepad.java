36a49184f185ac5a1dc355ac1cb00be437a508d6
Megg Gawat <megawatt@live.com>
2017-07-29 12:08:34 -0400

    Added file button to the menu

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -13,4 +13,25 @@ public class Notepad extends JFrame implements ActionListener {
 	private MenuItem openFile = new MenuItem();
 	private MenuItem saveFile = new MenuItem();
 	private MenuItem close = new MenuItem();
+	
+	public Notepad() {
+		this.setSize(500, 300); // set initial size of the window
+		this.setTitle("Java Notepad"); // set the title of the window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // set the default close operation (exit when it gets closed)
+		this.textArea.setFont(new Font("Century Gothic", Font.BOLD, 12)); // sets a default font for the text area
+
+		// Add textArea to the content pane
+		this.getContentPane().setLayout(new BorderLayout()); // the BorderLayout bit makes it fill automatically
+		this.getContentPane().add(textArea);
+		
+		// Add our menu bar to the GUI
+		this.setMenuBar(this.menuBar);
+		this.menuBar.add(this.file); // adds the menu option to the menu
+		this.file.setLabel("File"); // Sets the label of the file menu option to "File"
+		this.openFile.setLabel("Open");
+		this.openFile.addActionListener(this); // add an action listener so we know when it's been clicked
+		this.openFile.setShortcut(new MenuShortcut(KeyEvent.VK_O, false)); //set a keyboard shortcut
+		this.file.add(this.openFile); // adds the file option to the menu
+	}
+	
 }
